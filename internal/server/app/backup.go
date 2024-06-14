ee4c085375ff6384cf0e598f89fdcbd2b32407f3
Stas Merzlyakov <merzliakov@gmail.com>
2024-06-14 17:46:41 +0400

    добавил тестов

--- internal/server/app/backup.go
+++ internal/server/app/backup.go
@@ -23,8 +23,11 @@ type backUper struct {
 
 func (bU *backUper) RestoreBackUp(ctx context.Context) error {
 	metrics, err := bU.formatter.Read(ctx)
-	if err != nil && !errors.Is(err, os.ErrNotExist) {
-		panic(err)
+	if err != nil {
+		if !errors.Is(err, os.ErrNotExist) {
+			panic(err)
+		}
+		return nil
 	}
 
 	return bU.storage.SetAllMetrics(ctx, metrics)
@@ -35,12 +38,15 @@ func (bU *backUper) DoBackUp(ctx context.Context) error {
 	action := domain.GetAction(1)
 	metrics, err := bU.storage.GetAllMetrics(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("backup err - getAllMetrics return err: %w", err)
+	}
+	if len(metrics) == 0 {
+		return nil
 	}
 	err = bU.formatter.Write(ctx, metrics)
 	if err != nil {
 		logger.Errorf(action, "error", fmt.Sprintf("backup error - %s", err.Error()))
-		return err
+		return fmt.Errorf("write backup err: %w", err)
 	}
 	return nil
 }
